c13ee690b36b2ae1aa00210b3c6b199c46dd8958
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-03-19 11:09:40 +0700

    Membuat project wo while looping

--- 185_DoWhile/185_DoWhile.cpp
+++ 185_DoWhile/185_DoWhile.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main()
 {
-	
+	int x;
+	srand(time(0));
+
+	x = rand() % 10;
+
+	cout << "Nilai awal : " << x << endl;
+
+	do
+	{
+		cout << "Perulangan Do..While" << endl;
+		x = rand() % 10;
+
+		cout << "Nilai x : " << x << endl;
+	} while (x > 4);
 }
